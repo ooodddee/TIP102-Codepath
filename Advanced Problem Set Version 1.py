5c1090c2db06a05f9fd66c118ffd54ca53699355
sun <1980987589@qq.com>
2025-09-21 16:12:29 -0700

    ad p1

--- Advanced Problem Set Version 1.py
+++ Advanced Problem Set Version 1.py
@@ -55,6 +55,9 @@ def remove_dupes(items):
   input: sorted array
   output: integer -> length of the modified array
   """
+  if not items:
+    return 0
+  
   l = 0
 
   for r in range(1, len(items)):
@@ -68,3 +71,89 @@ print(remove_dupes(items))
 
 items = ["extract of malt", "haycorns", "honey", "thistle"]
 print(remove_dupes(items))
+
+# Problem 4: Sort Array by Parity
+def sort_by_parity(nums):
+  """
+  input:  integer array
+  output: even + odd
+
+  even: not n % 2
+  odd: n % 2
+  """
+  if not nums or len(nums) == 1:
+    return nums
+  
+  l, r = 0, len(nums) - 1
+  while l < r:
+    if not nums[l] % 2:
+      l += 1
+    elif nums[r] % 2:
+      r += 1
+    else:
+      nums[l], nums[r] = nums[r], nums[l]
+      l += 1
+      r -= 1
+  return nums
+
+nums = [3, 1, 2, 4]
+print(sort_by_parity(nums))
+
+nums = [0]
+print(sort_by_parity(nums))
+
+# Problem 5: Container with Most Honey
+def most_honey(heights):
+  """
+  input: an integer array
+  output: integer -> max area
+
+  area -> height(min(left, right)) * length
+  move lower height pointer
+  """
+  if not heights:
+    return 0
+  
+  l, r = 0, len(heights) - 1
+  res = float('-inf')
+
+  while l <= r:
+    area = min(heights[l], heights[r]) * (r - l)
+    res = max(res, area)
+    if heights[l] < heights[r]:
+      l += 1
+    else:
+      r -= 1
+  return res
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(most_honey(height))
+
+height = [1, 1]
+print(most_honey(height))
+
+# Problem 6: Merge Intervals
+def merge_intervals(intervals):
+  """
+  input: 2d array
+  output: non-overlapping 2d array
+
+  end >= start: overlapping
+  overlapping [start, end], start -> min(start), end -> max(end)
+  """
+  res = []
+  res.append(intervals[0])
+
+  for start, end in intervals[1:]:
+    last = res[-1]
+    if last[1] >= start:
+      last[1] = max(end, last[1])
+    else:
+      res.append([start, end])
+  return res
+
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(merge_intervals(intervals))
+
+intervals = [[1, 4], [4, 5]]
+print(merge_intervals(intervals))
